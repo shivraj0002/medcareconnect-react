2246df9eecf98e4461fa2cb86a986445b97f74a8
Shivraj Dange <95290113+shivraj0002@users.noreply.github.com>
2023-10-20 09:17:13 +0530

    Update Testimonials.js
    updated doctor name

--- src/components/Home/Testimonials/Testimonials.js
+++ src/components/Home/Testimonials/Testimonials.js
@@ -42,10 +42,10 @@ const Testimonials = () => {
           <div style={{ background: "rgb(0 86 140 / 7%)" }}>
             <img
               alt=""
-              src={require("../../../assets/people/rupesh-kumar.jpg")}
+              src={"https://imgs.search.brave.com/PzngAPChR2G1EghyNpeb6l57-C-wwF0B_VXbrqZORFw/rs:fit:860:0:0/g:ce/aHR0cHM6Ly90My5m/dGNkbi5uZXQvanBn/LzA2LzE5LzI2LzQ2/LzM2MF9GXzYxOTI2/NDY4MF94MlBCZEdM/RjU0c0ZlN2tUQnRB/dlpuUHlYZ3ZhUncw/WS5qcGc"}
             />
             <div className="myCarousel">
-              <h3>Dr. Rupesh Kumar</h3>
+              <h3>Dr. Nitin Kumar</h3>
               <h4>Doctor</h4>
               <p>
                 Our experience with Vanshan Pharma has been outstanding. Their
